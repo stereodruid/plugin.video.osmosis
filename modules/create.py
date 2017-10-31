d4d4e3e42288a014cdc263e65b377050f0dd2094
gu3nter <r.rosenschon@gmx.de>
2017-10-31 14:31:56 +0100

    Merge branch 'future' into future-sqlite3-mysql

--- modules/create.py
+++ modules/create.py
@@ -364,8 +364,8 @@ def addMovies(contentList, strm_name='', strm_type='Other', provider="n.a"):
                     
                     if label and strm_name:                                              
                         label = stringUtils.cleanByDictReplacements(label)           
-                        if HIDE_tile_in_OV == "true" and not label.find("[OV]") == -1:   
-			                      get_title_with_OV = 0
+                        if HIDE_tile_in_OV == "true" and not label.find("[OV]") == -1:  
+                            get_title_with_OV = 0
                         else:
                             get_title_with_OV = 1
 
