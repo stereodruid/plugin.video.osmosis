3b8126eb8ee32638fe5d025424d13a6e846fb857
Thorsten <v112@gmx.de>
2017-11-30 15:07:04 +0100

    fix special characters filesystem

--- modules/kodiDB.py
+++ modules/kodiDB.py
@@ -18,6 +18,7 @@
 
 import os
 import sys
+import re
 import datetime
 from modules import guiTools
 from modules import fileSys
@@ -230,8 +231,9 @@ def writeSong(pathID, albumID, artist, songName, duration, track, fileModTime):
 
     selectQuery = ("SELECT idSong FROM song WHERE strTitle LIKE ?;")
     selectArgs =  (songName,)
+    songNameFile = re.sub('[\/:*?<>|!@#$"]', '', songName)
     insertQuery = ("INSERT INTO song (iYear, dateAdded, idAlbum, idPath, {}, strTitle, strFileName, iTrack, strGenres, iDuration, iTimesPlayed, iStartOffset, iEndOffset, userrating, comment, mood, votes) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);".format(artistCol))
-    insertArgs =  (dateYear, dateAdded, albumID, pathID, artist, songName, songName + ".strm", track, 'osmosis', duration, 0, 0, 0, 0, 'osmosis', 'osmosis', 0,)
+    insertArgs =  (dateYear, dateAdded, albumID, pathID, artist, songName, songNameFile + ".strm", track, 'osmosis', duration, 0, 0, 0, 0, 'osmosis', 'osmosis', 0,)
     
     return manageDbRecord(selectQuery, selectArgs, insertQuery, insertArgs)
 
