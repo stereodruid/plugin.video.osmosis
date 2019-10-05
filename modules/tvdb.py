stereodruid/plugin.video.osmosis

commit ce4616b3a999f5cb7a2397a699de74a95541dfeb

Make sure ratio_max2 is never set to 0 as this would cause a division by zero error in an addon_log() call.

modules/tvdb.py
@@ -401,7 +401,7 @@ def findEpisodeByName(token, show_data, episodeSeason, episodeNr, episodeName, l
 
                         if ratio > ratio_max:
                             if ratio_max > 0 and not (ratio_max_season == episode.get('airedSeason') and ratio_max_episode == episode.get('airedEpisodeNumber')):
-                                ratio_max2 = ratio_max
+                                ratio_max2 = max(ratio,0.1)
                             ratio_max = ratio
                             ratio_max_season = episode.get('airedSeason')
                             ratio_max_episode = episode.get('airedEpisodeNumber')
@@ -409,7 +409,7 @@ def findEpisodeByName(token, show_data, episodeSeason, episodeNr, episodeName, l
                                             'episodeName': episode.get('episodeName'), 'match_ratio': ratio }
                         else:
                             if (ratio_max2 == 100 or ratio_max2 < ratio) and not (ratio_max_season == episode.get('airedSeason') and ratio_max_episode == episode.get('airedEpisodeNumber')):
-                                ratio_max2 = ratio
+                                ratio_max2 = max(ratio,0.1)
 
                         if ratio_max > 99.0:
                             break

commit ff45283889a6e0c59e0ba4249de9d69152bcdca9

Fix error introduced by commit ce4616b3a999f5cb7a2397a699de74a95541dfeb

modules/tvdb.py
@@ -401,7 +401,7 @@ def findEpisodeByName(token, show_data, episodeSeason, episodeNr, episodeName, l
 
                         if ratio > ratio_max:
                             if ratio_max > 0 and not (ratio_max_season == episode.get('airedSeason') and ratio_max_episode == episode.get('airedEpisodeNumber')):
-                                ratio_max2 = max(ratio,0.1)
+                                ratio_max2 = ratio_max
                             ratio_max = ratio
                             ratio_max_season = episode.get('airedSeason')
                             ratio_max_episode = episode.get('airedEpisodeNumber')